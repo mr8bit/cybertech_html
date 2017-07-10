e75b4711542f6e4e5fc5a8c6d3f65e68ca0e5a40
mr8bit <geniusRu1led>
2017-07-11 01:15:24 +0300

    Update template

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,5 @@
-$(function () {
-    $('#nav-icon3').click(function () {
-        $(this).toggleClass('open');
-        $(".sidenav").toggleClass('open-side');
-        $("body").toggleClass('no-scroll');
-    });
-    $('.border').click(function () {
-        $(this).toggleClass('active');
-        document.getElementById("order").value = $(this).data("order")+ " ";
-    });
-});
+$(function() {
 
+	// Custom JS
+
+});
